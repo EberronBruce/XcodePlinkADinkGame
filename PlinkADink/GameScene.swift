974b23afceba52f517b6c652e1bf1aa05dc13530
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-05 18:46:41 +0700

    Filled out goal areas

--- PlinkADink/GameScene.swift
+++ PlinkADink/GameScene.swift
@@ -13,8 +13,9 @@ class GameScene: SKScene {
     var discs = [SKSpriteNode]()
     var remainingLabel = SKLabelNode()
     var scoreLabel = SKLabelNode()
-    let numberOfDiscs = 20
+    let numberOfDiscs = 5
     var topPeg : CGPoint = CGPoint(x: 0, y: 0)
+    let postWidth = CGFloat(10)
     
     override func didMoveToView(view: SKView) {
         
@@ -23,6 +24,7 @@ class GameScene: SKScene {
         
         makeTopLabels()
         makeWallsAndGround()
+        makePosts()
         makeGoals()
         makePegs(10, numberOfRows: 12, pegRadius: 5, rowSpacing: 40, yStart: 120)
     }
@@ -106,41 +108,93 @@ class GameScene: SKScene {
         
     }
     
-    //Creating the goals
-    func makeGoals(){
+    //Creating the posts
+    func makePosts(){
         //Setting the height and with of the posts
-        let postWidth = CGFloat(10)
+        
         let postHeight = CGFloat(80)
         
         //setting up the first post
-        let post1 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: postWidth, height: postHeight))
+        let post1 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: self.postWidth, height: postHeight))
         post1.position = CGPoint(x: self.frame.size.width * 0.25, y: postHeight/2)
-        post1.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: postWidth, height: postHeight))
+        post1.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.postWidth, height: postHeight))
         post1.physicsBody?.dynamic = false
         self.addChild(post1)
         
         //setting up the second post
-        let post2 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: postWidth, height: postHeight))
+        let post2 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: self.postWidth, height: postHeight))
         post2.position = CGPoint(x: self.frame.size.width * 0.45, y: postHeight/2)
-        post2.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: postWidth, height: postHeight))
+        post2.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.postWidth, height: postHeight))
         post2.physicsBody?.dynamic = false
         self.addChild(post2)
         
         //setting up the third post
-        let post3 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: postWidth, height: postHeight))
+        let post3 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: self.postWidth, height: postHeight))
         post3.position = CGPoint(x: self.frame.size.width * 0.55, y: postHeight/2)
-        post3.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: postWidth, height: postHeight))
+        post3.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.postWidth, height: postHeight))
         post3.physicsBody?.dynamic = false
         self.addChild(post3)
         
         //setting up the fourth post
-        let post4 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: postWidth, height: postHeight))
+        let post4 = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: self.postWidth, height: postHeight))
         post4.position = CGPoint(x: self.frame.size.width * 0.75, y: postHeight/2)
-        post4.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: postWidth, height: postHeight))
+        post4.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.postWidth, height: postHeight))
         post4.physicsBody?.dynamic = false
         self.addChild(post4)
         
         
+    }
+    
+    //function used to make goals
+    func makeGoals() {
+        let goalHeight = CGFloat(80)
+        
+        let bigGoalWidth = (self.frame.size.width * 0.25) - (self.postWidth / 2)
+        let mediumGoalWidth = (self.frame.size.width * 0.2) - (self.postWidth)
+        let smallGoalWidth = (self.frame.size.width * 0.1) - (self.postWidth)
+        let  yPosition = goalHeight / 2
+        
+        //setting up the first goal
+        let goal1 = SKSpriteNode(color: UIColor.purpleColor(), size: CGSize(width: bigGoalWidth, height: goalHeight))
+        var xPosition = (bigGoalWidth / 2)
+        goal1.position = CGPoint(x: xPosition, y: yPosition)
+        goal1.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: bigGoalWidth, height: goalHeight))
+        goal1.physicsBody?.dynamic = false
+        self.addChild(goal1)
+        
+        //setting up the second goal
+        let goal2 = SKSpriteNode(color: UIColor.purpleColor(), size: CGSize(width: mediumGoalWidth, height: goalHeight))
+        xPosition = bigGoalWidth + self.postWidth + (mediumGoalWidth / 2)
+        goal2.position = CGPoint(x: xPosition, y: yPosition)
+        goal2.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: mediumGoalWidth, height: goalHeight))
+        goal2.physicsBody?.dynamic = false
+        self.addChild(goal2)
+        
+        //setting up the third goal
+        let goal3 = SKSpriteNode(color: UIColor.purpleColor(), size: CGSize(width: smallGoalWidth, height: goalHeight))
+        xPosition = bigGoalWidth + (2 * self.postWidth) + mediumGoalWidth + (smallGoalWidth / 2)
+        goal3.position = CGPoint(x: xPosition , y: yPosition)
+        goal3.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: smallGoalWidth, height: goalHeight))
+        goal3.physicsBody?.dynamic = false
+        self.addChild(goal3)
+        
+        //setting up the fourth goal
+        let goal4 = SKSpriteNode(color: UIColor.purpleColor(), size: CGSize(width: mediumGoalWidth, height: goalHeight))
+        xPosition = bigGoalWidth + (3 * self.postWidth) + mediumGoalWidth + smallGoalWidth + (mediumGoalWidth / 2)
+        goal4.position = CGPoint(x: xPosition, y: yPosition)
+        goal4.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: mediumGoalWidth, height: goalHeight))
+        goal4.physicsBody?.dynamic = false
+        self.addChild(goal4)
+        
+        //setting up the fifth goal
+        let goal5 = SKSpriteNode(color: UIColor.purpleColor(), size: CGSize(width: bigGoalWidth, height: goalHeight))
+        xPosition = bigGoalWidth + (4 * self.postWidth) + (2 * mediumGoalWidth) + smallGoalWidth + (bigGoalWidth / 2)            
+        goal5.position = CGPoint(x: xPosition, y: yPosition)
+        goal5.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: bigGoalWidth, height: goalHeight))
+        goal5.physicsBody?.dynamic = false
+        self.addChild(goal5)
+
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
